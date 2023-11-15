d024a97af213c7b5e86a93095dacba4318a97846
TakutoHashimoto <tkthsmt0531@gmail.com>
2023-11-15 22:36:17 +0900

    Add concurrent_futures_2.py

---
+++ python/concurrent_futures_2.py
@@ -0,0 +1,40 @@
+"""
+並列処理を行うメソッドに引数や戻り値がある場合
+"""
+
+from concurrent.futures import ThreadPoolExecutor
+import time
+
+
+def func_1() -> None:
+    for n in range(3):
+        time.sleep(2)
+        print(f"func_1 - {n}")
+
+    return "結果1"
+
+def func_2(x: str, y: str) -> str:
+    for n in range(3):
+        time.sleep(1)
+        print(f"func_2 - {n} ({x}, {y})")
+
+    return "結果2"
+
+def main():
+    print("開始")
+
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        future_1 = executor.submit(func_1)
+        future_2 = executor.submit(func_2, "あ", "A")
+
+    result_1 = future_1.result()
+    result_2 = future_2.result()
+
+    print(f"result_1: {result_1}")
+    print(f"result_2: {result_2}")
+
+    print("終了")
+
+
+if __name__ == "__main__":
+    main()
